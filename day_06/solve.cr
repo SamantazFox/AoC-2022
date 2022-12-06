137971d35a5d866f05c26980f89344178f8af859
Samantaz Fox <coding@samantaz.fr>
2022-12-06 22:48:42 +0100

    Solve part 2 of day 06

--- day_06/solve.cr
+++ day_06/solve.cr
@@ -5,16 +5,32 @@ end
 
 stream = input.split('\n', remove_empty: true)[0]
 
-start = -1
+packet_start = -1
 
 (stream.size - 4).times do |i|
   bytes = stream.byte_slice(i, 4).split("").uniq
 
   if bytes.size == 4
-    start = i+4
+    packet_start = i+4
     break
   end
 end
 
-puts "Part1 :"
-pp start
+puts "Part 1:"
+pp packet_start
+
+
+message_start = -1
+
+(stream.size - 14).times do |i|
+  bytes = stream.byte_slice(i, 14).split("").uniq
+
+  if bytes.size == 14
+    message_start = i+14
+    break
+  end
+end
+
+
+puts "\nPart 2:"
+pp message_start
