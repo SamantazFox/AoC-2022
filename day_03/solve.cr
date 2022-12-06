89353a379ce4e827c9a43186f6d0f8681965beae
Samantaz Fox <coding@samantaz.fr>
2022-12-06 18:42:37 +0100

    Solve part 2 of day 03

--- day_03/solve.cr
+++ day_03/solve.cr
@@ -2,12 +2,19 @@ input = File.open("#{__DIR__}/input.txt") do |file|
   file.gets_to_end
 end
 
-rucksacks = input.split('\n', remove_empty: true).map do |rs|
+
+groups = [] of Array(String)
+
+rucksacks = input.split('\n', remove_empty: true).map_with_index do |rs, i|
   itm_count = rs.size // 2
   content_1 = rs[0..(itm_count-1)].chars
   content_2 = rs[itm_count..-1].chars
 
   common = content_1 & content_2
+
+  grp = (i // 3)
+  groups << [] of String if !groups[grp]?
+  groups[grp] << rs
 
   {
     content_1: content_1,
@@ -37,3 +44,16 @@ end
 
 puts "Part 1:"
 pp total
+
+
+badges_priorities = groups.map do |grp|
+  a, b, c = grp.map(&.chars)
+  common = (a & b & c)
+  priority(common[0])
+end
+
+total_2 = badges_priorities.sum
+
+puts ""
+puts "Part 2:"
+pp total_2
